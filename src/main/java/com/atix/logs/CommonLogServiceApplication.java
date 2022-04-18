1e3128b12ee62609c959fce7717cfd5adda356d8
noelink <36247599+noelink@users.noreply.github.com>
2022-04-18 10:50:41 -0500

    Add files via upload
    first commit

---
+++ src/main/java/com/atix/logs/CommonLogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.atix.logs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonLogServiceApplication.class, args);
+	}
+
+}
